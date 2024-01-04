22f60a8b43b5ac6ea2368899533323d3e92543c1
oscar13545 <oscar.herediam10@gmail.com>
2024-01-04 17:49:46 -0600

    FistUpdate

--- Board/src/App.js
+++ Board/src/App.js
@@ -1,22 +1,16 @@
-import './App.css';
-import Board from './Components/Board/Board';
+import "./App.css";
+import Board from "./Components/Board/Board";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-
     <Router>
-        <Routes>
-
-          <Route exact path="/home" element={<Board/>}/>
-          <Route exact path="/" element={<Board/>}/>
-        </Routes>
+      <Routes>
+        <Route exact path="/home" element={<Board />} />
+        <Route exact path="/" element={<Board />} />
+      </Routes>
     </Router>
-    
   );
 }
 
